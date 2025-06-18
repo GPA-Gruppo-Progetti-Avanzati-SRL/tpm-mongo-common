0623fc73367ba200e408c9b4b067d6f57ad8370b
M.A.I <mario.imperato@gpagroup.it>
2025-06-18 10:08:15 +0200

    Aggiornamento jobs

--- jobs/driver/metrics.go
+++ jobs/driver/metrics.go
@@ -3,11 +3,14 @@ package driver
 import (
 	_ "embed"
 	"github.com/GPA-Gruppo-Progetti-Avanzati-SRL/tpm-common/util/promutil"
+	"github.com/rs/zerolog/log"
 	"gopkg.in/yaml.v3"
 )
 
 //go:embed jobs-metrics.yaml
 var jobMetrics []byte
+
+const JobMetricsGroupId = "jobs"
 
 func JobMetrics(jobMetricsGroupId string) (promutil.MetricGroupConfig, error) {
 	var cfg promutil.MetricGroupConfig
@@ -18,4 +21,14 @@ func JobMetrics(jobMetricsGroupId string) (promutil.MetricGroupConfig, error) {
 
 	cfg.GroupId = jobMetricsGroupId
 	return cfg, nil
+}
+
+func MustJobMetrics(jobMetricsGroupId string) promutil.MetricGroupConfig {
+	const semLogContext = "driver::job-metrics"
+	cfg, err := JobMetrics(jobMetricsGroupId)
+	if err != nil {
+		log.Fatal().Err(err).Str("jobMetricsGroupId", jobMetricsGroupId).Msg(semLogContext)
+	}
+
+	return cfg
 }
