b868f34b06f203d6a1075b6789fa9415a95dc612
Nambalorian <mario.imperato@gpagroup.it>
2025-12-22 22:42:07 +0100

    aggiornamento jobs

--- jobs/driver/driver.go
+++ jobs/driver/driver.go
@@ -54,7 +54,7 @@ func NewDriver(cfg *Config, wg *sync.WaitGroup) (*Driver, error) {
 }
 
 func (m *Driver) Start(sh chan error) error {
-	const semLogContext = "monitor::start"
+	const semLogContext = "driver::start"
 	m.shutdownChannel = sh
 	m.wg.Add(1)
 	go m.workLoop()
@@ -62,13 +62,13 @@ func (m *Driver) Start(sh chan error) error {
 }
 
 func (m *Driver) Close() error {
-	const semLogContext = "monitor::close"
+	const semLogContext = "driver::close"
 	close(m.quitc)
 	return nil
 }
 
 func (m *Driver) workLoop() {
-	const semLogContext = "monitor::work-loop"
+	const semLogContext = "driver::work-loop"
 
 	startedTasks := m.findAndStartTasks()
 	hasTasks := len(startedTasks) > 0
